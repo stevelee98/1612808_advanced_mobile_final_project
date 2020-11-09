fe3f565f07ee78fb865b52f99004138b4238468c
hoangvietle2598 <viet.lh.2598@gmail.com>
2020-11-10 00:21:28 +0700

    Add color, font size, common style, image, error code

---
+++ src/values/colors.js
@@ -0,0 +1,16 @@
+export const Colors = {
+    COLOR_PRIMARY: '#7ac8e5',
+    COLOR_TEXT_PRIMARY: '#57bde3',
+    COLOR_PRIMARY_DARK: '#7ddcff',
+    COLOR_PRIMARY_LIGHT: '#7ddcff',
+    COLOR_BACKGROUND: '#E9E9E9',
+    COLOR_WHITE: '#ffffff',
+    COLOR_BLACK: '#000000',
+    COLOR_BLACK_OPACITY_30: 'rgba(00, 00, 00, 0.3)',
+    COLOR_TEXT: '#000000',
+    COLOR_BLACK_OPACITY_50: 'rgba(00, 00, 00, 0.5)',
+    COLOR_DRK_GREY: '#959DAD',
+    COLOR_DRK_OPACITY: '#282828',
+    COLOR_GREY_LIGHT: '#cdcdcd',
+    COLOR_SHADOW: '#F2F2F6',
+}
---
+++ src/values/configConstants.js
@@ -0,0 +1,2 @@
+export const configConstants = {
+}
